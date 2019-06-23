dc5d722d5277979defb5e177e4e73789f7301673
sattvikee <sattvikee@users.noreply.github.com>
2019-06-23 22:48:27 +0530

    v1 without Redux

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quia, voluptates, libero id tenetur, nihil eaque illum perspiciatis quis harum aperiam quae! Quasi, impedit perspiciatis dignissimos ut rem animi nam voluptas incidunt expedita commodi, illum eaque dolorem ad libero voluptate excepturi distinctio ab accusamus consequuntur amet culpa velit itaque labore. Libero error atque inventore dolorum ipsam temporibus voluptate maiores minima consectetur nobis est ipsa cupiditate, quae neque blanditiis distinctio minus doloribus eveniet natus? Expedita obcaecati officia veniam neque impedit quo sequi odio ipsum hic ab sint fugit, vel vero incidunt, laudantium natus ea tempora qui assumenda. Aspernatur quaerat mollitia molestias modi corrupti consequatur neque nulla et! Ex mollitia molestias facilis. Modi a sit alias, atque optio itaque eius excepturi deserunt, placeat animi temporibus? Tempore deserunt id, quae mollitia, quibusdam deleniti enim quod placeat inventore temporibus obcaecati pariatur doloremque. Debitis, doloremque doloribus. Laudantium ipsa sint quos minus, iusto hic repellat cupiditate! Dolore error beatae aliquam repellat deleniti at nostrum est perspiciatis.</p>
+        </div>
+    )
+}
+
+export default Rainbow(About)
